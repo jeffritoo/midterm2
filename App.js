1513e9a2542836e5f02a13b768040bd89180597d
jeffritoo <jeffryvdavid@gmail.com>
2021-06-10 18:54:09 -0700

    jeff

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  return (
+
+  const [number, onChangeNumber] = React.useState(0);
+  const [message, setMessage] = React.useState("");
+ 
+
+    function getSquare() {
+      const squared = number * number;
+      return `The square of ${number} is ${squared}`;
+
+    }
+
+    return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Input a number:</Text>
+      <TextInput 
+      onChangeText={onChangeNumber}
+      value={number}
+      keyboardType="numeric"
+      />
+
+      <Button title="Calc Square" onPress={() =>setMessage(getSquare())}/>
+
       <StatusBar style="auto" />
+      <Text>{message}</Text>
     </View>
   );
 }
