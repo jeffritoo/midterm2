e47190654ca3fce3a6156cbbf0eb4b7597ddc50d
jeffritoo <jeffryvdavid@gmail.com>
2021-06-10 20:50:57 -0700

    final final fina

--- styles.js
+++ styles.js
@@ -1,23 +1,45 @@
-// import { StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 
-// const styles = StyleSheet.create({
-//     box: {
-//         backgroundColor: 'teal',
-//         height:125,
-//         width:200,
-//         alignItems: 'center',
-//         justifyContent: 'center',
-//         borderRadius:10,
-//       },
-//       boxText: {
-//         color: "white",
-//         fontSize:30,
-//       },
-//       boxTextSmall: {
-//         color: "white",
-//         fontSize:10,
-//       },
-// });
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    textInput:{
+      width: 200,
+      borderColor:"gray",
+      borderWidth:1,
+      borderStyle:"solid",
+      borderRadius:3,
+      margin:5,
+  },
+    text:{
+      alignSelf:"stretch",
+  },
+    button:{
+     backgroundColor:"#fff",
+     borderColor:"blue",
+  },
+  box: {
+    backgroundColor: 'teal',
+    height:125,
+    width:200,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius:10,
+  },
+  boxText: {
+    color: "white",
+    fontSize:30,
+  },
+  boxTextSmall: {
+    color: "white",
+    fontSize:10,
+  },
+  });
+  
 
-// export default styles;
+export default styles;
 
